843c5ad694578d11bb04013ebecfae6909f78322
Hen <hen@flamefew.org>
2026-07-06 16:32:50 -0700

    Added FTLA

---
+++ docs/data/ftla.js
@@ -0,0 +1,152 @@
+window.JUMPSTART_SETS = window.JUMPSTART_SETS || {};
+window.JUMPSTART_SETS["ftla"] = {
+  "setCode": "ftla",
+  "setName": "Avatar: The Last Airbender Beginner Box Front Cards",
+  "sourceUrl": "https://scryfall.com/search?q=e%3Aftla&include_extras=true",
+  "generatedAt": "2026-07-06T23:32:20.193218+00:00",
+  "packCount": 10,
+  "ownedPackCount": 10,
+  "hasOwnershipFile": true,
+  "packs": [
+    {
+      "id": "a5663076-9524-4c40-803a-8f98cb18534d",
+      "collectorNumber": "1",
+      "name": "Allies",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/a/5/a5663076-9524-4c40-803a-8f98cb18534d.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/1/allies?utm_source=api"
+    },
+    {
+      "id": "463f98d4-4fd0-41fd-895f-f2d415a6b79f",
+      "collectorNumber": "2",
+      "name": "Aang Tutorial",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/4/6/463f98d4-4fd0-41fd-895f-f2d415a6b79f.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/2/aang-tutorial?utm_source=api"
+    },
+    {
+      "id": "1c9206e6-7c49-4041-a214-13c91b7a0e19",
+      "collectorNumber": "3",
+      "name": "Spells",
+      "colorCodes": [
+        "U"
+      ],
+      "colorLabel": "Blue",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/1/c/1c9206e6-7c49-4041-a214-13c91b7a0e19.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/3/spells?utm_source=api"
+    },
+    {
+      "id": "3fdb11c1-be75-46b5-93ef-54ab20420d59",
+      "collectorNumber": "4",
+      "name": "Waterbending",
+      "colorCodes": [
+        "U"
+      ],
+      "colorLabel": "Blue",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/3/f/3fdb11c1-be75-46b5-93ef-54ab20420d59.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/4/waterbending?utm_source=api"
+    },
+    {
+      "id": "35853e40-4c48-4b5a-90b4-3844189e4a57",
+      "collectorNumber": "5",
+      "name": "Attacking",
+      "colorCodes": [
+        "B"
+      ],
+      "colorLabel": "Black",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/3/5/35853e40-4c48-4b5a-90b4-3844189e4a57.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/5/attacking?utm_source=api"
+    },
+    {
+      "id": "0411ae1f-a1f8-4e64-8cdd-96b8350db1b2",
+      "collectorNumber": "6",
+      "name": "Counters",
+      "colorCodes": [
+        "B"
+      ],
+      "colorLabel": "Black",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/0/4/0411ae1f-a1f8-4e64-8cdd-96b8350db1b2.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/6/counters?utm_source=api"
+    },
+    {
+      "id": "b3458dbe-037b-480a-82b2-d2fc5bb4e120",
+      "collectorNumber": "7",
+      "name": "Firebending",
+      "colorCodes": [
+        "R"
+      ],
+      "colorLabel": "Red",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/b/3/b3458dbe-037b-480a-82b2-d2fc5bb4e120.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/7/firebending?utm_source=api"
+    },
+    {
+      "id": "103548bd-3d30-478e-b653-f38a04af60bd",
+      "collectorNumber": "8",
+      "name": "Zuko Tutorial",
+      "colorCodes": [
+        "R"
+      ],
+      "colorLabel": "Red",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/1/0/103548bd-3d30-478e-b653-f38a04af60bd.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/8/zuko-tutorial?utm_source=api"
+    },
+    {
+      "id": "057b47c1-0a5b-46ec-b7ff-4ca4a52ee082",
+      "collectorNumber": "9",
+      "name": "Big Creatures",
+      "colorCodes": [
+        "G"
+      ],
+      "colorLabel": "Green",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/0/5/057b47c1-0a5b-46ec-b7ff-4ca4a52ee082.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/9/big-creatures?utm_source=api"
+    },
+    {
+      "id": "5fe6cc53-16f3-4a3f-946e-48527cc97ae6",
+      "collectorNumber": "10",
+      "name": "Earthbending",
+      "colorCodes": [
+        "G"
+      ],
+      "colorLabel": "Green",
+      "isMulticolor": false,
+      "isOwned": true,
+      "excludedFromRandomizer": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/5/f/5fe6cc53-16f3-4a3f-946e-48527cc97ae6.jpg",
+      "scryfallUri": "https://scryfall.com/card/ftla/10/earthbending?utm_source=api"
+    }
+  ]
+};
