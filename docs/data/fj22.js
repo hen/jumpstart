800f33e073d8086c1bd0c13d07eac72a83a3693e
Hen <hen@flamefew.org>
2026-07-06 16:09:56 -0700

    Adding J22

---
+++ docs/data/fj22.js
@@ -0,0 +1,178 @@
+window.JUMPSTART_SETS = window.JUMPSTART_SETS || {};
+window.JUMPSTART_SETS["fj22"] = {
+  "setCode": "fj22",
+  "setName": "Jumpstart 2022 Front Cards",
+  "sourceUrl": "https://scryfall.com/search?q=e%3Afj22&include_extras=true",
+  "generatedAt": "2026-07-06T23:09:36.307172+00:00",
+  "packCount": 14,
+  "packs": [
+    {
+      "id": "26da8a11-ae74-44c4-b52b-304feefd3ace",
+      "collectorNumber": "4",
+      "name": "Holy",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/2/6/26da8a11-ae74-44c4-b52b-304feefd3ace.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/4/holy?utm_source=api"
+    },
+    {
+      "id": "67391db5-8f46-46ef-8a69-ce1456f29d95",
+      "collectorNumber": "6",
+      "name": "Law",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/6/7/67391db5-8f46-46ef-8a69-ce1456f29d95.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/6/law?utm_source=api"
+    },
+    {
+      "id": "79676ee8-b5fc-4216-8372-8666edae42a7",
+      "collectorNumber": "7",
+      "name": "Spirits",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/7/9/79676ee8-b5fc-4216-8372-8666edae42a7.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/7/spirits?utm_source=api"
+    },
+    {
+      "id": "ceef0331-9b5b-4578-b361-749d326244d5",
+      "collectorNumber": "8",
+      "name": "Teamwork",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/c/e/ceef0331-9b5b-4578-b361-749d326244d5.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/8/teamwork?utm_source=api"
+    },
+    {
+      "id": "2c21e5b2-97d5-43a6-b46e-b550defbfca5",
+      "collectorNumber": "9",
+      "name": "Vehicles",
+      "colorCodes": [
+        "W"
+      ],
+      "colorLabel": "White",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/2/c/2c21e5b2-97d5-43a6-b46e-b550defbfca5.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/9/vehicles?utm_source=api"
+    },
+    {
+      "id": "8a55b3e7-be27-4ba8-a7b2-0f165f842652",
+      "collectorNumber": "10",
+      "name": "Detective",
+      "colorCodes": [
+        "U"
+      ],
+      "colorLabel": "Blue",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/8/a/8a55b3e7-be27-4ba8-a7b2-0f165f842652.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/10/detective?utm_source=api"
+    },
+    {
+      "id": "d44274b6-b8d0-421c-96ff-ae3b03204bce",
+      "collectorNumber": "14",
+      "name": "Merfolk",
+      "colorCodes": [
+        "U"
+      ],
+      "colorLabel": "Blue",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/d/4/d44274b6-b8d0-421c-96ff-ae3b03204bce.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/14/merfolk?utm_source=api"
+    },
+    {
+      "id": "3536118d-d525-4184-9cb9-cf2480a3a208",
+      "collectorNumber": "15",
+      "name": "Scrying",
+      "colorCodes": [
+        "U"
+      ],
+      "colorLabel": "Blue",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/3/5/3536118d-d525-4184-9cb9-cf2480a3a208.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/15/scrying?utm_source=api"
+    },
+    {
+      "id": "849ba60b-fb0c-45bb-8d82-94e70089f815",
+      "collectorNumber": "18",
+      "name": "Think Again",
+      "colorCodes": [
+        "U"
+      ],
+      "colorLabel": "Blue",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/8/4/849ba60b-fb0c-45bb-8d82-94e70089f815.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/18/think-again?utm_source=api"
+    },
+    {
+      "id": "16e06e66-a2dd-42e8-bf65-1e23a3fd3ce0",
+      "collectorNumber": "19",
+      "name": "Boneyard",
+      "colorCodes": [
+        "B"
+      ],
+      "colorLabel": "Black",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/1/6/16e06e66-a2dd-42e8-bf65-1e23a3fd3ce0.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/19/boneyard?utm_source=api"
+    },
+    {
+      "id": "dac9dfaf-5877-44c0-a7f6-d0ba5477c24d",
+      "collectorNumber": "22",
+      "name": "Fangs",
+      "colorCodes": [
+        "B"
+      ],
+      "colorLabel": "Black",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/d/a/dac9dfaf-5877-44c0-a7f6-d0ba5477c24d.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/22/fangs?utm_source=api"
+    },
+    {
+      "id": "0e55eba6-afd6-4af3-8cfb-d19ac13b179c",
+      "collectorNumber": "27",
+      "name": "Zombies",
+      "colorCodes": [
+        "B"
+      ],
+      "colorLabel": "Black",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/0/e/0e55eba6-afd6-4af3-8cfb-d19ac13b179c.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/27/zombies?utm_source=api"
+    },
+    {
+      "id": "9799f443-6dac-42cb-9e0d-f91695b4a97f",
+      "collectorNumber": "35",
+      "name": "Spicy",
+      "colorCodes": [
+        "R"
+      ],
+      "colorLabel": "Red",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/9/7/9799f443-6dac-42cb-9e0d-f91695b4a97f.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/35/spicy?utm_source=api"
+    },
+    {
+      "id": "86fc74b1-577c-4ffe-96f4-5a1a1777b5c7",
+      "collectorNumber": "45",
+      "name": "Wolves",
+      "colorCodes": [
+        "G"
+      ],
+      "colorLabel": "Green",
+      "isMulticolor": false,
+      "imageUrl": "https://cards.scryfall.io/normal/front/8/6/86fc74b1-577c-4ffe-96f4-5a1a1777b5c7.jpg",
+      "scryfallUri": "https://scryfall.com/card/fj22/45/wolves?utm_source=api"
+    }
+  ]
+};
